620c9c6326f2fee013de4e056723f182bba8d7f8
고범석 <kobums23@gmail.com>
2024-04-04 20:54:15 +0900

    매출 담당자 화면 추가

--- src/views/management/Statistics.vue
+++ src/views/management/Statistics.vue
@@ -1,11 +1,33 @@
 <template>
   <Title title="매출 보고서" />
 
-  <div style="display: flex; gap: 5px; margin-bottom: 10px">
-    <el-date-picker style="margin: 0px 0px; height: 24px; width: 150px" v-model="data.search.startdate" />
-    <el-date-picker style="margin: 0px 0px; height: 24px; width: 150px" v-model="data.search.enddate" />
-    <el-button size="small" class="filter-item" type="primary" @click="clickSearch">검색</el-button>
-  </div>
+  <el-descriptions class="margin-top" :column="3" border style="margin-bottom: 10px">
+    <el-descriptions-item>
+      <template #label>
+        <div style="display: flex; justify-content: space-between">
+          <p>담당자</p>
+          <div style="flex: 1; display: flex; align-items: center; justify-content: right">
+            <el-icon :size="20" @click="clickVisible"><UserFilled /></el-icon>
+          </div>
+        </div>
+      </template>
+      <div style="width: 300px">
+        <span v-for="item in data.manager" :key="item.id" style="margin-right: 5px">{{ item.name }}</span>
+      </div>
+    </el-descriptions-item>
+    <el-descriptions-item>
+      <template #label>
+        <div style="text-align: center">기간</div>
+      </template>
+      <div style="display: flex; justify-content: space-between">
+        <div>
+          <el-date-picker style="margin: 0px 0px; height: 24px; width: 150px" v-model="data.search.startdate" />
+          <el-date-picker style="margin: 0px 0px; height: 24px; width: 150px" v-model="data.search.enddate" />
+        </div>
+        <el-button size="small" class="filter-item" type="primary" @click="clickSearch">검색</el-button>
+      </div>
+    </el-descriptions-item>
+  </el-descriptions>
 
   <div style="margin-top: 10px; margin-bottom: 20px; display: flex; justify-content: space-evenly">
     <el-card style="float: left; width: 480px; padding-top: 0px" shadow="never">
@@ -67,6 +89,37 @@
     </el-table-column>
   </el-table>
 
+  <el-dialog v-model="data.visible" width="800px">
+    <div style="display: flex; justify-content: space-between; gap: 5px; margin-bottom: 10px">
+      <el-input v-model="data.searchMaster" placeholder="검색할 내용을 입력해 주세요" style="width: 300px" @keyup.enter.native="getMasters" />
+      <el-button size="small" class="filter-item" type="primary" @click="getMasters">검색</el-button>
+    </div>
+    <el-table :data="data.masters" border :height="height(170)" ref="listRef" @selection-change="changeList" style="height: 600px">
+      <el-table-column type="selection" width="40" align="center" />
+      <el-table-column prop="loginid" label="로그인아이디" align="left" />
+      <el-table-column prop="name" label="이름" align="left" width="80" />
+      <el-table-column label="팀" align="left" width="100">
+        <template #default="scope">
+          {{ getDepartment(scope.row.department) }}
+        </template>
+      </el-table-column>
+      <el-table-column prop="email" label="이메일" align="left" />
+      <el-table-column prop="tel" label="연락처" align="left" width="100" />
+      <el-table-column label="주소" align="left">
+        <template #default="scope"> {{ scope.row.address }} {{ scope.row.addressetc }} </template>
+      </el-table-column>
+      <el-table-column label="권한" align="center" width="70">
+        <template #default="scope">
+          {{ getLevel(scope.row.level) }}
+        </template>
+      </el-table-column>
+    </el-table>
+    <template #footer>
+      <el-button size="small" @click="clickCancelMaster">취소</el-button>
+      <el-button size="small" type="primary" @click="clickSubmitMaster">등록</el-button>
+    </template>
+  </el-dialog>
+
   <div style="margin-top: 10px; display: flex; justify-content: space-between">
     <el-button v-if="data.mode == 'month'" size="small" type="success" @click="clickBackMonth">뒤로</el-button>
     <el-button v-if="data.mode == 'day'" size="small" type="success" @click="clickBackDay">뒤로</el-button>
@@ -75,10 +128,10 @@
 </template>
 
 <script setup lang="ts">
-import { reactive, onMounted } from 'vue'
+import { ref, reactive, onMounted } from 'vue'
 import router from '~/router'
 import { util, size } from '~/global'
-import { Statistics, Statisticsyear, Statisticsmonth, Statisticsday, Billing } from '~/models'
+import { Statistics, Statisticsyear, Statisticsmonth, Statisticsday, Billing, User, Department } from '~/models'
 import { Chart, Grid, Line, Bar } from 'vue3-charts'
 import { useStore } from 'vuex'
 
@@ -112,6 +165,10 @@ const data = reactive({
     enddate: '2024-04-04',
   },
   charts: [],
+  departments: [],
+  searchMaster: '',
+  masters: [],
+  manager: [],
 })
 
 async function getItems() {
@@ -184,6 +241,57 @@ async function getBills() {
   data.items = items
 }
 
+async function getDepartments() {
+  let res = await Department.find({
+    name: data.search.text,
+    page: data.page,
+    pagesize: data.pagesize,
+    company: store.getters.getUser.company,
+    orderby: 'de_parent desc, de_order, de_name',
+  })
+
+  if (res.items == null) {
+    res.items = []
+  }
+
+  let items = []
+
+  for (let i = 0; i < res.items.length; i++) {
+    let item = res.items[i]
+
+    item.index = i + 1
+    items.push(item)
+  }
+
+  data.departments = items
+}
+
+async function getMasters() {
+  let res = await User.find({
+    name: data.searchMaster,
+    page: 0,
+    // pagesize: data.masterpagesize,
+    company: store.getters.getUser.company,
+    approval: 3,
+    status: 1,
+    orderby: 'u_id desc',
+  })
+
+  if (res.items == null) {
+    res.items = []
+  }
+
+  let items = []
+
+  for (let i = 0; i < res.items.length; i++) {
+    let item = res.items[i]
+    item.index = i + 1
+    items.push(item)
+  }
+
+  data.masters = items
+}
+
 function clickSearch() {
   getItems()
 }
@@ -246,6 +354,7 @@ const handleClose = (done: () => void) => {
 onMounted(async () => {
   util.loading(true)
 
+  await getDepartments()
   await getItems()
 
   util.loading(false)
@@ -316,5 +425,61 @@ const getSummary = (param: SummaryMethodProps) => {
   })
 
   return sums
+}
+
+const listRef = ref<InstanceType<typeof ElTable>>()
+const listSelection = ref([])
+const toggleListSelection = rows => {
+  if (rows) {
+    rows.forEach(row => {
+      listRef.value!.toggleRowSelection(row, undefined)
+    })
+  } else {
+    listRef.value!.clearSelection()
+  }
+}
+const changeList = val => {
+  listSelection.value = val
+}
+
+function clickVisible() {
+  util.loading(true)
+  getMasters()
+  util.loading(false)
+  data.visible = true
+}
+
+function clickSubmitMaster() {
+  data.manager = []
+  for (let i = 0; i < listSelection.value.length; i++) {
+    let value = listSelection.value[i]
+
+    data.manager.push(value)
+  }
+
+  clickCancelMaster()
+}
+
+function clickCancelMaster() {
+  data.searchMaster = ''
+  data.visible = false
+}
+
+function getLevel(id) {
+  if (id > 5) {
+    return ''
+  }
+
+  return User.levels[id]
+}
+
+function getDepartment(id) {
+  let items = data.departments.filter(item => item.id == id)
+
+  if (items.length == 0) {
+    return ''
+  }
+
+  return items[0].name
 }
 </script>
